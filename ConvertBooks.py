c798e0d87284700a26574826d221cf2838350aa0
TheWicklowWolf <111055425+TheWicklowWolf@users.noreply.github.com>
2024-06-17 11:35:32 +0100

    Added Source and Destination folders

--- ConvertBooks.py
+++ ConvertBooks.py
@@ -16,47 +16,61 @@ def convert_book(input_file, output_file):
         logging.error(f"Error converting {input_file}: {str(e)}")
 
 
-def convert_books():
-    if not os.path.exists(path_to_books):
-        logging.error(f"The directory {path_to_books} does not exist.")
-        logging.error(f"Sleeping...")
-        return
-
+def run_conversion():
     with concurrent.futures.ThreadPoolExecutor(max_workers=thread_limit) as executor:
         futures = []
 
-        for root, _, files in os.walk(path_to_books):
-            book_files = [file for file in files if any(file.endswith(format) for format in desired_formats)]
+        for root, dirs, files in os.walk(source_folder):
+            if not dirs and not files:
+                logging.error(f"The directory {source_folder} is empty.")
+                return
+            book_files = [file for file in files if any(file.endswith(format) for format in book_source_formats)]
             unique_books = set([os.path.splitext(book)[0] for book in book_files])
 
             for book in unique_books:
-                for format in desired_formats:
+                for format in book_source_formats:
                     book_path = os.path.join(root, f"{book}{format}")
                     if os.path.exists(book_path):
                         input_file = os.path.join(book_path)
                         break
 
-                for file_format in desired_formats:
-                    output_file = os.path.join(root, f"{book}{file_format}")
+                for file_format in desired_output_formats:
+                    relative_root = os.path.relpath(root, source_folder)
+                    output_file_dir = os.path.join(destination_folder, relative_root)
+                    if not os.path.exists(output_file_dir):
+                        os.makedirs(output_file_dir, exist_ok=True)
+                    output_file = os.path.join(output_file_dir, f"{book}{file_format}")
                     if not os.path.exists(output_file):
                         futures.append(executor.submit(convert_book, input_file, output_file))
 
         concurrent.futures.wait(futures)
 
+    logging.info(f"Conversion Complete, sleeping...")
+
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
-path_to_books = os.getenv("path_to_books", "path/to/books")
-desired_formats = os.getenv("desired_formats", ".epub,.mobi,.azw3").split(",")
+book_source_formats = os.getenv("book_source_formats", ".epub,.mobi,.azw3").split(",")
+desired_output_formats = os.getenv("desired_output_formats", ".mobi,.azw3").split(",")
 schedule = os.getenv("schedule", "0").split(",")
 schedule = [int(hour.strip()) for hour in schedule]
 run_at_startup = os.getenv("run_at_startup", "false")
 thread_limit = int(os.getenv("thread_limit", 1))
 
+source_folder = "source"
+if not os.path.exists(source_folder):
+    os.makedirs(source_folder, exist_ok=True)
+
+destination_folder = "destination"
+if not os.path.exists(destination_folder):
+    os.makedirs(destination_folder, exist_ok=True)
+
 if run_at_startup.lower() == "true":
-    convert_books()
+    run_conversion()
+    sleep(3600)
+
 while True:
     if datetime.now().hour in schedule:
-        convert_books()
+        run_conversion()
     else:
         logging.info(f"Current hour is {datetime.now().hour}. Not a scheduled run hour. Sleeping...")
     sleep(3600)
--- README.md
+++ README.md
@@ -12,7 +12,8 @@ services:
     image: thewicklowwolf/convertbooks:latest
     container_name: convertbooks
     volumes:
-      - path/to/books:path/to/books
+      - /path/to/books_source:/convertbooks/source
+      - /path/to/books_destination:/convertbooks/destination
       - /etc/localtime:/etc/localtime:ro
     restart: unless-stopped
 ```
@@ -21,11 +22,11 @@ services:
 
 Certain values can be set via environment variables:
 
-* __path_to_books__: The actual ebook folder path. Defaults to `path/to/books`.
-* __desired_formats__: The desired ebook formats. Defaults to `.epub,.mobi,.azw3`.
-* __schedule__: The time to run the conversions. Defaults to `0`.
-* __run_at_startup__: Wether to run at startup. Defaults to `False`.
-* __thread_limit__: Max number of threads to run. Defaults to `1`.
+* __book_source_formats__: Defines the file formats to search for in the source folder. Defaults to `.epub,.mobi,.azw3`.
+* __desired_output_formats__: Specifies the formats to convert eBooks into. Defaults to `.mobi,.azw3`.
+* __schedule__: Sets the times for the conversion process to run, specified in hours. Defaults to `0`.
+* __run_at_startup__: Controls whether the conversion process should initiate automatically at startup. Defaults to `False`.
+* __thread_limit__: Specifies the maximum number of concurrent threads for running conversion tasks. Defaults to `1`.
 
 ---
 
